069d97555f9241509bbb5b81fdeb7eab9a784e04
oksanakolomenko <oksana.kolomenko@student.htw-berlin.de>
2025-12-22 17:16:49 +0100

    improver UI

--- src/pages/models.py
+++ src/pages/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from django.db.models import Max
 
-from django.db import models
 
 class TeamMember(models.Model):
     nickname = models.CharField(max_length=50)
@@ -11,6 +11,12 @@ class TeamMember(models.Model):
 
     class Meta:
         ordering = ['order']
+
+    def save(self, *args, **kwargs):
+        if self.order == 0:
+            max_order = TeamMember.objects.aggregate(Max('order'))['order__max']
+            self.order = (max_order or 0) + 1
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.nickname} ({self.position})"
--- src/pages/views.py
+++ src/pages/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 
+from menu.models import Category
 from pages.models import TeamMember
 
 
 def home(request):
-    team_members = TeamMember.objects.all()  # Or filter if needed
+    team_members = TeamMember.objects.all()
+    menu_categories = Category.objects.prefetch_related(
+        'subcategories__menu_items'
+    ).all()
+
+    # Filter out categories and subcategories without items
+    filtered_categories = []
+    for category in menu_categories:
+        # Keep subcategories that have at least one menu item
+        subcategories_with_items = [sub for sub in category.subcategories.all() if sub.menu_items.exists()]
+        if subcategories_with_items:
+            # Assign filtered subcategories back to category
+            category.subcategories_filtered = subcategories_with_items
+            filtered_categories.append(category)
+
     return render(request, "home.html", {
-        "team_members": team_members
+        "team_members": team_members,
+        "menu_categories": filtered_categories
     })
 
 
